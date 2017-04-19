36d72d64e4e47d8ff2ef5840447c80946debf95f
Gabriel Erdelyi <erdgabios@gmail.com>
2017-04-20 00:48:37 +0200

    hamburger menu nem forog jol, de mukodik

--- javascript.js
+++ javascript.js
@@ -2,3 +2,8 @@ $('#exCollapsingNavbar').on('click', 'a:not(.dropdown-toggle)', function (e) {
     $('#exCollapsingNavbar.in').collapse('hide');
 });
 
+var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
