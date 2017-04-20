ae53ce88a43910c98ddc6668da35570b02704619
Gabriel Erdelyi <erdgabios@gmail.com>
2017-04-20 09:02:51 +0200

    hamburger menu animacio mukodik, hurra

--- javascript.js
+++ javascript.js
@@ -1,10 +1,17 @@
-$('#exCollapsingNavbar').on('click', 'a:not(.dropdown-toggle)', function (e) {
-    $('#exCollapsingNavbar.in').collapse('hide');
-});
+//$('#exCollapsingNavbar').on('click', 'a:not(.dropdown-toggle)', function (e) {
+//    $('#exCollapsingNavbar.in').collapse('hide');
+//    
+//});
 
 var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
     // Do something else, like open/close menu
   });
+
+$('#exCollapsingNavbar').on('click', 'a:not(.dropdown-toggle)', function (e) {
+    $('#exCollapsingNavbar.in').collapse('hide');
+    $hamburger.toggleClass("is-active");
+    
+});
 
