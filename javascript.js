c2439dd26e9ff38bf1c839429b2fb5a2e8b07d0d
Gabriel Erdelyi <erdgabios@gmail.com>
2017-04-20 22:59:38 +0200

    OK

--- javascript.js
+++ javascript.js
@@ -9,9 +9,16 @@ var $hamburger = $(".hamburger");
     // Do something else, like open/close menu
   });
 
+//var $logo = $("#logo");
+//  $logo.on("click", function(e) {
+//    $('#exCollapsingNavbar.in').collapse('hide');  
+//    $logo.toggleClass("is-active");
+//    // Do something else, like open/close menu
+//  });
+
 $('#exCollapsingNavbar').on('click', 'a:not(.dropdown-toggle)', function (e) {
     $('#exCollapsingNavbar.in').collapse('hide');
     $hamburger.toggleClass("is-active");
-    
+    //$logo.toggleClass("is-active");
 });
 
